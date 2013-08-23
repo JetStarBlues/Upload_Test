3fd7f362b14b9e515ca9bbf770c33fc6a2d67e33
JetStarBlues <JetStarBlues@users.noreply.github.com>
2013-08-23 14:02:34 -0600

    initial commit

---
+++ Gemfile
@@ -0,0 +1,20 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.0'
+gem 'gravatar_image_tag','0.1.0'
+
+group :development, :test do 
+	gem 'sqlite3-ruby', '1.2.5', :require => 'sqlite3' 
+	gem 'rspec-rails', '2.4.0'    
+end
+
+group :test do
+	gem 'nokogiri', '1.3.3'
+	gem 'webrat', '0.5.3'
+	gem 'autotest', '4.4.6'
+	gem 'factory_girl_rails','1.0'
+end
+
+group :production do 
+	gem 'pg'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UploadTest::Application.config.secret_token = '721298b2be27b166e9d9ff97ae5710092baa7baae132b913efc1eaf60aa3213eef8ecd070820d3bb2abc55c53a636323b54a2a338b1e6b585cd4bf1e9e9e647c'
